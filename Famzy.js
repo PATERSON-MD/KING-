61cfce30a84db35afb493a52a4ebafaf80f1e1cf
kervens <romeochefbratva200k@gmail.com>
2025-11-18 04:56:08 -0500

    Add files via upload

---
+++ Famzy.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7722248068:AAE8YjQfxVilJA8lw5eKiSi3RfNjEDb_Ous", // Token bot Telegram
+  OWNER_ID: "7188042729", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
